fc716cc692aa7f768ea236671df6e491a07b3eea
Eduardo Henrique <eduhenriquesrs@gmail.com>
2021-12-10 21:07:14 -0300

    Utilizando ViewSet e Routers

--- app/cursos/urls.py
+++ app/cursos/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-
+from rest_framework.routers import SimpleRouter
 from .views import *
+
+router = SimpleRouter()
+router.register('cursos', CursoViewSet)
+router.register('avaliacoes', AvaliacaoViewSet)
 
 urlpatterns = [
 
